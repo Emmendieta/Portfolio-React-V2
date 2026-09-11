2b495481f6e660e7f000184e5cb30912ce093f60
asrivas <asrivas@issys.gov.ar>
2026-09-11 10:17:51 -0300

    finding the error to connect to DB

--- backend/helpers/dbConnect.helper.js
+++ backend/helpers/dbConnect.helper.js
@@ -3,6 +3,11 @@ import { connect } from "mongoose";
 const dbConnect = async (link) => {
     try {
         await connect(link);
+        console.log(
+            "Mongo URI:",
+            link.replace(/\/\/([^:]+):([^@]+)@/, "//$1:****@")
+        );
+
         console.warn("Connect To MongoDB");
     } catch (error) {
         throw new Error(`Error connectiong MongoDB: ${error.message}`)
