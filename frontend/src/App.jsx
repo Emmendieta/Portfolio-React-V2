cd3040e451d3be83cf2141404afc647bb1ddfd26
asrivas <asrivas@issys.gov.ar>
2026-09-22 08:40:12 -0300

    implementing permissions on components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -73,184 +73,58 @@ function AppRoutes() {
         <Route path="/login" element={<Login />} />
 
         <Route path='/people' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_people"} >
-            <PeopleList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/people/form/:id' element={<PeopleForm />} />
+          <ProtectedRoutes permissionResolver={() => "read_all_people"} > <PeopleList /> </ProtectedRoutes> } />
+        <Route path='/people/form/:id' element={<PeopleForm />} /> <Route path='/users' element={ <ProtectedRoutes permissionResolver={() => "read_all_users"} > <UsersList /> </ProtectedRoutes> } />
+        <Route path="/users/form/:id" element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_users": "update_users" }> <UsersForm /> </ProtectedRoutes> } />
 
-        <Route path='/users' element={
-          /*  <ProtectedRoutes permissionResolver={() => "read_all_users"} > */
-          <UsersList />
-          /* </ProtectedRoutes> */
-        } />
-        <Route path="/users/form/:id" element={
-          /* <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_users": "update_users" }> */
-          <UsersForm />
-          /* </ProtectedRoutes> */
-        } />
+        <Route path='/cities' element={ <ProtectedRoutes permissionResolver={() => "read_all_cities"}> <CitiesList /> </ProtectedRoutes> } />
+        <Route path='/cities/form/:id' element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_cities" : "update_cities"}> <CitiesForm /> </ProtectedRoutes> } />
 
-        <Route path='/cities' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_cities"}>
-            <CitiesList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/cities/form/:id' element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_cities" : "update_cities"}>
-            <CitiesForm />
-          </ProtectedRoutes>
-        } />
+        <Route path="/provinces" element={ <ProtectedRoutes permissionResolver={() => "read_all_provinces"}> <ProvincesList /> </ProtectedRoutes> } />
+        <Route path="/provinces/form/:id" element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_provinces" : "update_provinces"}> <ProvincesForm /> </ProtectedRoutes> } />
 
-        <Route path="/provinces" element={
-          <ProtectedRoutes permissionResolver={() => "read_all_provinces"}>
-            <ProvincesList />
-          </ProtectedRoutes>
-        } />
-        <Route path="/provinces/form/:id" element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_provinces" : "update_provinces"}>
-            <ProvincesForm />
-          </ProtectedRoutes>
-        } />
+        <Route path="/countries" element={ <ProtectedRoutes permissionResolver={() => "read_all_countries"}> <CountriesList /> </ProtectedRoutes> } />
+        <Route path="/countries/form/:id" element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_countries" : "update_countries"} > <CountriesForm /> </ProtectedRoutes> } />
 
-        <Route path="/countries" element={
-          <ProtectedRoutes permissionResolver={() => "read_all_countries"}>
-            <CountriesList />
-          </ProtectedRoutes>
-        } />
-        <Route path="/countries/form/:id" element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_countries" : "update_countries"} >
-            <CountriesForm />
-          </ProtectedRoutes>
-        } />
+        <Route path="/continents" element={ <ProtectedRoutes permissionResolver={() => "read_all_continents"}> <ContinentsList /> </ProtectedRoutes> } />
+        <Route path="/continents/form/:id" element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_continents" : "update_continents"}> <ContinentsForm /> </ProtectedRoutes> } />
 
-        <Route path="/continents" element={
-          <ProtectedRoutes permissionResolver={() => "read_all_continents"}>
-            <ContinentsList />
-          </ProtectedRoutes>
-        } />
-        <Route path="/continents/form/:id" element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_continents" : "update_continents"}>
-            <ContinentsForm />
-          </ProtectedRoutes>
-        } />
+        <Route path="/permissions" element={ <ProtectedRoutes permissionResolver={() => "read_all_permissions"}> <PermissionsList /> </ProtectedRoutes> } />
+        <Route path="/permissions/form/:id" element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_permissions" : "update_permissions"}> <PermissionsForm /> </ProtectedRoutes>} />
 
-        <Route path="/permissions" element={
-          <ProtectedRoutes permissionResolver={() => "read_all_permissions"}>
-            <PermissionsList />
-          </ProtectedRoutes>
-        } />
-        <Route path="/permissions/form/:id" element={
-          /*<ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_permissions" : "update_permissions"}>*/
-            <PermissionsForm />
-          /*</ProtectedRoutes>*/}
-        />
+        <Route path="/roles" element={ <ProtectedRoutes permissionResolver={() => "read_all_roles"}> <RoleList /> </ProtectedRoutes> } />
+        <Route path="/roles/form/:id" element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_roles" : "update_roles"}> <RoleForm /> </ProtectedRoutes> } />
 
-        <Route path="/roles" element={
-          <ProtectedRoutes permissionResolver={() => "read_all_roles"}>
-            <RoleList />
-          </ProtectedRoutes>
-        } />
-        <Route path="/roles/form/:id" element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_roles" : "update_roles"}>
-            <RoleForm />
-          </ProtectedRoutes>
-        } />
+        <Route path='/habilities' element={ <ProtectedRoutes permissionResolver={() => "read_all_habilities"}> <HabilitiesList /> </ProtectedRoutes> } />
+        <Route path='/habilities/form/:id' element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_habilities" : "update_habilities"} > <HabilitiesForm /> </ProtectedRoutes> } />
 
-        <Route path='/habilities' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_habilities"}>
-            <HabilitiesList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/habilities/form/:id' element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_habilities" : "update_habilities"} >
-            <HabilitiesForm />
-          </ProtectedRoutes>
-        } />
+        <Route path='/responsibilities' element={ <ProtectedRoutes permissionResolver={() => "read_all_responsibilities"}> <ResponsibilitiesList /> </ProtectedRoutes> } />
+        <Route path='/responsibilities/form/:id' element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_responsibilities" : "update_responsibilities"}> <ResponsibilitiesForm /> </ProtectedRoutes> } />
 
-        <Route path='/responsibilities' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_responsibilities"}>
-            <ResponsibilitiesList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/responsibilities/form/:id' element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_responsibilities" : "update_responsibilities"}>
-            <ResponsibilitiesForm />
-          </ProtectedRoutes>
-        } />
-
-        <Route path='/categories' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_categories"}>
-            <CategoriesList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/categories/form/:id' element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_categories" : "update_categories"}>
-            <CategoriesForm />
-          </ProtectedRoutes>
-        } />
+        <Route path='/categories' element={ <ProtectedRoutes permissionResolver={() => "read_all_categories"}> <CategoriesList /> </ProtectedRoutes> } />
+        <Route path='/categories/form/:id' element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_categories" : "update_categories"}> <CategoriesForm /> </ProtectedRoutes> } />
         <Route path='/categories/many/form' element={<CategoriesCreateManyForm />} />
-        <Route path='/categories/reorder' element={<CategoriesOrder /> } />
+        <Route path='/categories/reorder' element={ <ProtectedRoutes permissionResolver={() => "reoder_categories"}> <CategoriesOrder /> </ProtectedRoutes>} />
 
-        <Route path='/educations' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_educations"}>
-            <EduactionsList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/educations/form/:id' element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_educations" : "create_educations"}>
-            <EducationsForm />
-          </ProtectedRoutes>
-        } />
-        <Route path='/educations/reorder' element={<EducationsOrder /> } />
+        <Route path='/educations' element={<ProtectedRoutes permissionResolver={() => "read_all_educations"}> <EduactionsList /> </ProtectedRoutes> } />
+        <Route path='/educations/form/:id' element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_educations" : "create_educations"}> <EducationsForm /> </ProtectedRoutes> } />
+        <Route path='/educations/reorder' element={ <ProtectedRoutes permissionResolver={() => "reorder_educations"}> <EducationsOrder /> </ProtectedRoutes> } />
 
-        <Route path='/proyects' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_proyects"}>
-            <ProyectsList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/proyects/form/:id' element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_proyects" : "update_proyects"}>
-            <ProyectsForm />
-          </ProtectedRoutes>
-        } />
-        <Route path='proyects/reorder' element={ <ProyectsOrder /> } />
+        <Route path='/proyects' element={ <ProtectedRoutes permissionResolver={() => "read_all_proyects"}> <ProyectsList /> </ProtectedRoutes> } />
+        <Route path='/proyects/form/:id' element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_proyects" : "update_proyects"}> <ProyectsForm /> </ProtectedRoutes> } />
+        <Route path='proyects/reorder' element={ <ProtectedRoutes permissionResolver={() => "reorder_proyects"}> <ProyectsOrder /> </ProtectedRoutes> } />
 
-        <Route path='/skills' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_skills"}>
-            <SkillsList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/skills/form/:id' element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_skills" : "update_skills"} >
-            <SkillsForm />
-          </ProtectedRoutes>
-        } />
-        <Route path='/skills/reorder' element={<SkillsOrder /> } />
+        <Route path='/skills' element={ <ProtectedRoutes permissionResolver={() => "read_all_skills"}> <SkillsList /> </ProtectedRoutes> } />
+        <Route path='/skills/form/:id' element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_skills" : "update_skills"} > <SkillsForm /> </ProtectedRoutes> } />
+        <Route path='/skills/reorder' element={ <ProtectedRoutes permissionResolver={() => "reorder_skills"}> <SkillsOrder /> </ProtectedRoutes> } />
 
+        <Route path='/socials' element={ <ProtectedRoutes permissionResolver={() => "read_all_socials"}> <SocialsList /> </ProtectedRoutes> } />
+        <Route path='/socials/form/:id' element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_socials" : "update_socials"}> <SocialsForm /> </ProtectedRoutes> } />
+        <Route path='/socials/reorder' element={ <ProtectedRoutes permissionResolver={() => "reorder_socials"}> <SocialOrder /> </ProtectedRoutes> } />
 
-        <Route path='/socials' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_socials"}>
-            <SocialsList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/socials/form/:id' element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_socials" : "update_socials"}>
-            <SocialsForm />
-          </ProtectedRoutes>
-        } />
-        <Route path='/socials/reorder' element={<SocialOrder /> } />
-
-        <Route path='/works' element={
-          <ProtectedRoutes permissionResolver={() => "read_all_works"}>
-            <WorksList />
-          </ProtectedRoutes>
-        } />
-        <Route path='/works/form/:id' element={
-          <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_works" : "update_works"}>
-            <WorksForm />
-          </ProtectedRoutes>
-        } />
-        <Route path='/works/reorder' element={<WorksOrder /> } />
+        <Route path='/works' element={ <ProtectedRoutes permissionResolver={() => "read_all_works"}> <WorksList /> </ProtectedRoutes> } />
+        <Route path='/works/form/:id' element={ <ProtectedRoutes permissionResolver={({ id }) => id === "new" ? "create_works" : "update_works"}> <WorksForm /> </ProtectedRoutes> } />
+        <Route path='/works/reorder' element={ <ProtectedRoutes permissionResolver={() => "reorder_works"}> <WorksOrder /> </ProtectedRoutes> } />
 
         <Route path='/forbidden' element={<Forbidden />} />
         <Route path='*' element={<NotFound />} />
