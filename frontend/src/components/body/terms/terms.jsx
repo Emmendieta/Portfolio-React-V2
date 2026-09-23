58d3256beade52cb5416c95f652a89480115bcf5
asrivas <asrivas@issys.gov.ar>
2026-09-23 08:13:34 -0300

    fixing bugs on terms and adding styles

--- frontend/src/components/body/terms/terms.jsx
+++ frontend/src/components/body/terms/terms.jsx
@@ -2,6 +2,7 @@ import { LANG_CONST } from "../../../constants/SelectLang.Constant";
 import { useLanguage } from "../../../context/Language.Context";
 import H1Fields from "../generalFields/h1Fields/h1fields";
 import H2Fields from "../generalFields/h2Fields/h2Fields";
+import "./terms.css";
 
 function Terms() {
     const { language } = useLanguage();
@@ -10,89 +11,89 @@ function Terms() {
     return (
         <div className="termsCont">
             <section className="termsSectCont">
-                <H1Fields value={TEXT.TERM_COND_USE} language={language}
-                    clH1Cont="" clH1Text="" />
+                <H1Fields value={`${TEXT.TERM_COND_USE}`} language={language}
+                    clH1Cont="termsH1Cont" clH1Text="termsH1" />
                 <H2Fields value={`${TEXT.LAST_UPDATE} ${TEXT.DATE_UPDATE}`} language={language}
-                    className="" classNameH2="" />
-                <p className="terms_p">{TEXT.WELCOME_TO} <strong><a href="https://www.emmendieta.com">emmendieta.com</a></strong> ({TEXT.THE_WEBSITE}). {TEXT.ACCEPTING_WEB} COREGIR EL TIPO EN H2FIELDS</p>
+                    className="termsH2Cont" classNameH2="termsH2" />
+                <p className="termsP">{TEXT.WELCOME_TO} <strong><a href="https://www.emmendieta.com">emmendieta.com</a></strong> ({TEXT.THE_WEBSITE}). {TEXT.ACCEPTING_WEB}</p>
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.PURPOSE} language={language}
-                    className="" classNameH2="" />
-                <H2Fields value={TEXT.PURPOSE_EXP} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.PURPOSE}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
+                <H2Fields value={TEXT.PUPROSE_EXP} language={language}
+                    className="termsH2Cont" classNameH2="termsH2" />
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.INTELLECTUAL_PROPERTY} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.INTELLECTUAL_PROPERTY}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
                 <H2Fields value={TEXT.INTELLECTUAL_PROPERTY_EXP_1} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
                 <H2Fields value={TEXT.INTELLECTUAL_PROPERTY_EXP_2} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.ACEPTABLE_USE} language={language}
-                    className="" classNameH2="" />
-                <H2Fields value={TEXT.ACEPTABLE_USE_EXP} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.ACCEPTABLE_USE}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
+                <H2Fields value={TEXT.ACCEPTABLE_USE_EXP} language={language}
+                    className="termsH2Cont" classNameH2="termsH2" />
                 <ul className="termsUl">
-                    <li className="termsLi">{TEXT.ACEPTABLE_USE_LI_1}</li>
-                    <li className="termsLi">{TEXT.ACEPTABLE_USE_LI_2}</li>
-                    <li className="termsLi">{TEXT.ACEPTABLE_USE_LI_3}</li>
+                    <li className="termsLi">{TEXT.ACCEPTABLE_USE_LI_1}</li>
+                    <li className="termsLi">{TEXT.ACCEPTABLE_USE_LI_2}</li>
+                    <li className="termsLi">{TEXT.ACCEPTABLE_USE_LI_3}</li>
                 </ul>
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.ACCURACY_INFO} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.ACCURACY_INFO}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
                 <H2Fields value={TEXT.ACCURACY_INFO_EXP_1} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
                 <H2Fields value={TEXT.ACCURACY_INFO_EXP_2} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.THIRD_PARTY} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.THIRD_PARTY}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
                 <H2Fields value={TEXT.THIRD_PARTY_EXP} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.LIMITATION_LIABILITY} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.LIMITATION_LIABILITY}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
                 <H2Fields value={TEXT.LIMITATION_LIABILITY_EXP} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.CONTACT_FORM} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.CONTACT_FORM}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
                 <H2Fields value={TEXT.CONTACT_FORM_EXP_1} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
                 <H2Fields value={TEXT.CONTACT_FORM_EXP_2} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.PRIVACY} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.PRIVACY}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
                 <H2Fields value={TEXT.PRIVACY_EXP} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.CHANGE_TERMS} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.CHANGE_TERMS}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
                 <H2Fields value={TEXT.CHANGE_TERMS_EXP} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.GOV_LAW} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.GOV_LAW}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
                 <H2Fields value={TEXT.GOV_LAW_EXP} language={language}
-                    className="" classNameH2="" />
+                    className="termsH2Cont" classNameH2="termsH2" />
             </section>
             <section className="termsSectCont">
-                <H2Fields value={TEXT.CONTACT_TERM} language={language}
-                    className="" classNameH2="" />
+                <H2Fields value={`${TEXT.CONTACT_TERMS}:`} language={language}
+                    className="termsH2Cont" classNameH2="termsH2Title" />
                 <H2Fields value={TEXT.CONTACT_EXP_1} language={language}
-                    className="" classNameH2="" />
-                <p className="terms_p">Email:{" "} <a href="mailto:emmendieta12@gmail.com">emmendieta12@gmail.com</a> CAMBIAR DSPUES POR H2FIELD</p>
+                    className="termsH2Cont" classNameH2="termsH2" />
+                <p className="termsP">Email:{" "} <a href="mailto:emmendieta12@gmail.com">emmendieta12@gmail.com</a></p>
             </section>
         </div>
     );
