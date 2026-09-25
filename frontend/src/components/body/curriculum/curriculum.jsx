09771eba7d46110baaf36798f3042a2038eac24b
asrivas <asrivas@issys.gov.ar>
2026-09-25 08:49:10 -0300

    adding styles on educations from curriculum v2

--- frontend/src/components/body/curriculum/curriculum.jsx
+++ frontend/src/components/body/curriculum/curriculum.jsx
@@ -258,14 +258,14 @@ function Curriculum() {
                     ) : (<></>)}
                 </div>
                 <div className="currMidDivCont">
-                    <H1Fields value={`{${TEXT.ACADEMIC_BACKGROUND}:`} language={language}
+                    <H1Fields value={`${TEXT.ACADEMIC_BACKGROUND}:`} language={language}
                         clH1Cont="currMidH1Cont" clH1Text="currMidH1" />
                     {courses.length > 0 ? (
                         <div className="currFieldCont">
                             <H2Fields value={`${TEXT.COURSES}:`} language={language}
                                 className="currFieldH2SubtitleCont" classNameH2="currFieldH2Subtitle" />
                             {courses.map((course) => (
-                                <div key={course._id}>
+                                <div key={course._id} className="currFieldDetailCont">
                                     <H2Fields value={course.institutionName?.[language] || ""} language={language}
                                         className="currFieldH2Cont" classNameH2="currFieldH2Bold" />
                                     <H2Fields value={course.title?.[language] || ""} language={language}
@@ -277,11 +277,11 @@ function Curriculum() {
                         </div>
                     ) : (<></>)}
                     {universities.length > 0 ? (
-                        <div>
+                        <div className="currFieldCont">
                             <H2Fields value={`${TEXT.UNIVERSITIES}:`} language={language}
                                 className="currFieldH2SubtitleCont" classNameH2="currFieldH2Subtitle" />
                             {universities.map((uni) => (
-                                <div key={uni._id}>
+                                <div key={uni._id} className="currFieldDetailCont">
                                     <H2Fields value={uni.institutionName?.[language] || ""} language={language}
                                         className="currFieldH2Cont" classNameH2="currFieldH2Bold" />
                                     <H2Fields value={uni.title?.[language] || ""} language={language}
@@ -293,11 +293,11 @@ function Curriculum() {
                         </div>
                     ) : (<> </>)}
                     {highSchools.length > 0 ? (
-                        <div>
+                        <div className="currFieldCont">
                             <H2Fields value={`${TEXT.HIGH_SCHOOLS}:`} language={language}
                                 className="currFieldH2SubtitleCont" classNameH2="currFieldH2Subtitle" />
                             {highSchools.map((high) => (
-                                <div key={high._id}>
+                                <div key={high._id} className="currFieldDetailCont">
                                     <H2Fields value={high.institutionName?.[language] || ""} language={language}
                                         className="currFieldH2Cont" classNameH2="currFieldH2Bold" />
                                     <H2Fields value={high.title?.[language] || ""} language={language}
@@ -309,11 +309,11 @@ function Curriculum() {
                         </div>
                     ) : (<></>)}
                     {primarySchools.length > 0 ? (
-                        <div>
+                        <div className="currFieldCont">
                             <H2Fields value={`${TEXT.PRIMARY_SCHOOLS}:`} language={language}
                                 className="currFieldH2SubtitleCont" classNameH2="currFieldH2Subtitle" />
                             {primarySchools.map((primary) => (
-                                <div key={primary._id}>
+                                <div key={primary._id} className="currFieldDetailCont">
                                     <H2Fields value={primary.institutionName?.[language] || ""} language={language}
                                         className="currFieldH2Cont" classNameH2="currFieldH2Bold" />
                                     <H2Fields value={primary.title?.[language] || ""} language={language}
@@ -325,11 +325,11 @@ function Curriculum() {
                         </div>
                     ) : (<></>)}
                     {conferences.length > 0 ? (
-                        <div>
+                        <div className="currFieldCont">
                             <H2Fields value={`${TEXT.CONFERENCES}:`} language={language}
                                 className="currFieldH2SubtitleCont" classNameH2="currFieldH2Subtitle" />
                             {conferences.map((conf) => (
-                                <div key={conf._id}>
+                                <div key={conf._id} className="currFieldDetailCont">
                                     <H2Fields value={conf.institutionName?.[language] || ""} language={language}
                                         className="currFieldH2Cont" classNameH2="currFieldH2Bold" />
                                     <H2Fields value={conf.title?.[language] || ""} language={language}
@@ -341,11 +341,11 @@ function Curriculum() {
                         </div>
                     ) : (<></>)}
                     {others.length > 0 ? (
-                        <div>
+                        <div className="currFieldCont">
                             <H2Fields value={`${TEXT.OTHERS}:`} language={language}
                                 className="currFieldH2SubtitleCont" classNameH2="currFieldH2SubtitlecurrFieldH2" />
                             {others.map((other) => (
-                                <div key={other._id}>
+                                <div key={other._id} className="currFieldDetailCont">
                                     <H2Fields value={other.institutionName?.[language] || ""} language={language}
                                         className="currFieldH2Cont" classNameH2="currFieldH2Bold" />
                                     <H2Fields value={other.title?.[language] || ""} language={language}
